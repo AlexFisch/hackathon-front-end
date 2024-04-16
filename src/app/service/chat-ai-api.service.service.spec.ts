de5e7df20fabeebc11841bacc2ebba84c7facc46
AlexFisch <alexfischbein@Alexs-MacBook-Pro.local>
2024-04-16 09:34:47 -0400

    add chatbot component and api service

---
+++ src/app/service/chat-ai-api.service.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ChatAiApiServiceService } from './chat-ai-api.service.service';
+
+describe('ChatAiApiServiceService', () => {
+  let service: ChatAiApiServiceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ChatAiApiServiceService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
