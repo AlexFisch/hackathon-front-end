52fc543b2c2c7e6a1bb28c5aae993640b33e80f6
AlexFisch <alexfischbein@Alexs-MacBook-Pro.local>
2024-04-16 21:29:53 -0400

    fix some merge conflicts

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -2,17 +2,20 @@ import {Component, OnInit} from '@angular/core';
 import { ChatAiApiService } from '../service/chat-ai-api.service';
 import {DataService} from "../_services/data.service";
 import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chatbot',
   templateUrl: './chatbot.component.html',
   styleUrls: ['./chatbot.component.scss']
 })
-export class ChatbotComponent implements  OnInit{
+export class ChatbotComponent implements OnInit{
 
   userMessage !: string;
   botMessage !: string;
   chatHistory: {role: string, content: string}[] = [];
+  threadsArray!: Observable<string>[];
+  latestThread !: Observable<any>[];
 
   constructor(private aiService: ChatAiApiService,
               private dataService: DataService,
@@ -30,14 +33,36 @@ export class ChatbotComponent implements  OnInit{
   })
   }
 
+  getUserProfile(): void {
+    this.aiService.getUser("661e97780635815ce1c14eb5")
+      .subscribe(res => {
+        if (!res) {
+          this.router.navigate(['account/login']);
+        }
+        this.threadsArray = res.threads;
+        const latestThreadId = this.threadsArray[0];
+        console.log("here's the threads array" + this.threadsArray);
+        this.getLatestThread(res.threads[0]);
+      });
+  }
+
+  getLatestThread(threadsId: string): void {
+    this.aiService.getAllMessagesInThread(threadsId)
+        .subscribe(res => {
+          console.log("res from get latest thread: " + JSON.stringify(res));
+          const messageObjectArray = res.data;
+          let message = {};
+
+        })
+  }
   sendMessage() {
     const userMessage = this.userMessage;
     this.chatHistory.push({role: 'user', content: userMessage});
-    this.aiService.sendMessage(this.userMessage)
-        .subscribe(res => {
-          this.botMessage = res.reply;
-          this.chatHistory.push({role: 'assitant', content: this.botMessage});
-          this.userMessage = '';
-        });
+    // this.aiService.sendMessage("661e97780635815ce1c14eb5", this.userMessage)
+    //     .subscribe(res => {
+    //       this.botMessage = res.reply;
+    //       this.chatHistory.push({role: 'assistant', content: this.botMessage});
+    //       this.userMessage = '';
+    //     });
   }
 }
