9fb344d735eaaba4f0ecb2e2e5d8e1e34b952283
naresh1237 <naresh.java007@gmail.com>
2024-04-16 21:00:00 -0400

    initial home , login and registartion changes

--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -29,7 +29,7 @@ export class RegisterComponent implements OnInit {
         this.form = this.formBuilder.group({
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
-            username: ['', Validators.required],
+            //username: ['', Validators.required],
             email: ['', Validators.required],
             password: ['', [Validators.required, Validators.minLength(6)]]
         });
@@ -58,7 +58,7 @@ export class RegisterComponent implements OnInit {
             .subscribe({
                 next: () => {
                     this.alertService.success('Registration successful', { keepAfterRouteChange: true });
-                    this.router.navigate(['../login'], { relativeTo: this.route });
+                    this.router.navigate(['account/login'], { relativeTo: this.route });
                 },
                 error: error => {
                     this.alertService.error(error);
